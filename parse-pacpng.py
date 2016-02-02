f5df120ea84da0e29d318b3d4c286ff3327d1910
jimlee4530 <542243844@qq.com>
2016-02-02 12:41:29 +0800

    new_idea

--- parse-pacpng.py
+++ parse-pacpng.py
@@ -1,6 +1,29 @@
 from pcapng import FileScanner
+import os
 
-with open('4.pacpng') as fp:
-    scanner = FileScanner(fp)
-    for block in scanner:
-        pass  # do something with the block...
+# # with open('/home/jimlee/Documents/Git/simple-sql-inject-pacpng-detection-by-SVM/4.pcapng') as fp:
+# fp_r =  open('/home/jimlee/Documents/Git/simple-sql-inject-pacpng-detection-by-SVM/4.pcapng','r')
+# scanner = FileScanner(fp)
+# for block in scanner:
+#     print block
+#     num = raw_input('enter y/n:')
+#     if num = 'y':
+#     	print block
+#     else:
+#     	pass
+# fp.close
+
+
+
+fp_w = open('/home/jimlee/Documents/Git/simple-sql-inject-pacpng-detection-by-SVM/test.txt','a')
+fp_r =  open('/home/jimlee/Documents/Git/simple-sql-inject-pacpng-detection-by-SVM/4.pcapng','r')
+scanner = FileScanner(fp_r)
+i = 0
+for block in scanner:
+	# i += 1
+	# if i > 2:
+    fp_w.write("%s%s" % (block ,os.linesep))
+    # else:
+    # 	continue
+fp_w.close
+fp_r.close
